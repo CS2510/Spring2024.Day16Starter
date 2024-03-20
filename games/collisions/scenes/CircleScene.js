d598b9abbe9ca891aa064fe0ec77d9eae912cff9
B. Ricks <bricks@unomaha.edu>
2024-03-20 11:38:34 -0500

    Refactor collision game

--- games/collisions/scenes/CircleScene.js
+++ games/collisions/scenes/CircleScene.js
@@ -1,6 +1,6 @@
-import "./FollowMouseComponent.js"
-import "./CheckCollisionsComponentCircle.js"
-import "./CheckCollisionsComponentRectangle.js"
+import "../components/FollowMouseComponent.js"
+import "../components/CheckCollisionsComponentCircle.js"
+import "../components/CheckCollisionsComponentRectangle.js"
 
 class CircleScene extends Scene {
   constructor() {
@@ -12,21 +12,14 @@ class CircleScene extends Scene {
     let collisionCircleGameObject = new GameObject("CollisionCircleGameObject")
     collisionCircleGameObject.addComponent(new Circle())
     collisionCircleGameObject.addComponent(new CheckCollisionsComponentCircle())
-    collisionCircleGameObject.transform.x = 100;
-    collisionCircleGameObject.transform.y = 100;
-    collisionCircleGameObject.transform.scaleX = 50
-    GameObject.instantiate(collisionCircleGameObject);
+    GameObject.instantiate(collisionCircleGameObject, 100, 100, 50);
 
 
     //A rectangle against which we will do collision detection
     let collisionRectangleGameObject = new GameObject("CollisionRectangleGameObject")
     collisionRectangleGameObject.addComponent(new Rectangle("green", "transparent"))
     collisionRectangleGameObject.addComponent(new CheckCollisionsComponentRectangle())
-    collisionRectangleGameObject.transform.x = 200;
-    collisionRectangleGameObject.transform.y = 200;
-    collisionRectangleGameObject.transform.scaleX = 20
-    collisionRectangleGameObject.transform.scaleY = 30;
-    GameObject.instantiate(collisionRectangleGameObject);
+    GameObject.instantiate(collisionRectangleGameObject, 200, 200, 20, 30);
 
 
     // A infinitely small point that will follow the mouse cursor
--- games/collisions/scenes/RectangleScene.js
+++ games/collisions/scenes/RectangleScene.js
@@ -1,7 +1,7 @@
-import "./FollowMouseComponent.js"
-import "./CheckCollisionsComponentCircle_Rectangle.js"
-import "./CheckCollisionsComponentRectangle_Rectangle.js"
-import CheckCollisionsComponentRectangle_Rectangle from "./CheckCollisionsComponentRectangle_Rectangle.js"
+import "../components/FollowMouseComponent.js"
+import "../components/CheckCollisionsComponentCircle_Rectangle.js"
+import "../components/CheckCollisionsComponentRectangle_Rectangle.js"
+import CheckCollisionsComponentRectangle_Rectangle from "../components/CheckCollisionsComponentRectangle_Rectangle.js"
 
 class RectangleScene extends Scene {
   constructor() {
@@ -13,30 +13,21 @@ class RectangleScene extends Scene {
     let collisionCircleGameObject = new GameObject("CollisionCircleGameObject")
     collisionCircleGameObject.addComponent(new Circle())
     collisionCircleGameObject.addComponent(new CheckCollisionsComponentCircle_Rectangle())
-    collisionCircleGameObject.transform.x = 100;
-    collisionCircleGameObject.transform.y = 100;
-    collisionCircleGameObject.transform.scaleX = 50
-    GameObject.instantiate(collisionCircleGameObject);
+    GameObject.instantiate(collisionCircleGameObject, 100, 100, 50);
 
 
     //A rectangle against which we will do collision detection
     let collisionRectangleGameObject = new GameObject("CollisionRectangleGameObject")
     collisionRectangleGameObject.addComponent(new Rectangle("green", "transparent"))
     collisionRectangleGameObject.addComponent(new CheckCollisionsComponentRectangle_Rectangle())
-    collisionRectangleGameObject.transform.x = 200;
-    collisionRectangleGameObject.transform.y = 200;
-    collisionRectangleGameObject.transform.scaleX = 20
-    collisionRectangleGameObject.transform.scaleY = 30;
-    GameObject.instantiate(collisionRectangleGameObject);
+    GameObject.instantiate(collisionRectangleGameObject, 200, 200, 20, 30);
 
 
     // A infinitely small point that will follow the mouse cursor
     let mouseCursorGameObject = new GameObject("MouseCursorGameObject")
     mouseCursorGameObject.addComponent(new Rectangle())
     mouseCursorGameObject.addComponent(new FollowMouseComponent())
-    mouseCursorGameObject.transform.scaleX = 30;
-    mouseCursorGameObject.transform.scaleY = 30;
-    GameObject.instantiate(mouseCursorGameObject)
+    GameObject.instantiate(mouseCursorGameObject, 30, 30)
   }
 
 }
